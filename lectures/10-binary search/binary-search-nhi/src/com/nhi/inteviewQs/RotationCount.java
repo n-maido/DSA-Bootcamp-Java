b2ae9d6e91e4df71030eda389b11850b5a7b4056
n-maido <nhi_maido@outlook.com>
2022-01-16 23:08:31 -0800

    finish bs q's

--- lectures/10-binary search/binary-search-nhi/src/com/nhi/inteviewQs/RotationCount.java
+++ lectures/10-binary search/binary-search-nhi/src/com/nhi/inteviewQs/RotationCount.java
@@ -1,40 +1,21 @@
 package com.nhi.inteviewQs;
 
 /**
- * Given a sorted array that's rotated around an unknown pivot, find the target
- * https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * Given a sorted rotated array, count how many times it's been rotated
+ * https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
  */
-public class RotatedBS {
+public class RotationCount {
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2};
-        int target = 0;
-        System.out.println(search(nums, target));
+        System.out.println(countRotations(nums)); // alt + enter to auto create method
     }
 
-
-    static public int search(int[] nums, int target) {
+    private static int countRotations(int[] nums) {
         int pivot = findPivot(nums);
-
-        // if we don't find a pivot -> arr isn't rotated
-        if (pivot == -1) {
-            // do normal bs
-            return binarySearch(nums, target, 0, nums.length - 1);
-        }
-
-        // if pivot is found, we have 2 asc sorted arrs
-        // case 1:
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-        // case 2:
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1); // search LS
-        }
-        // case 3:
-        return binarySearch(nums, target, pivot = 1, nums.length - 1);
+        return pivot + 1; // if findPivot returns -1, it'll add up to 0
     }
 
-    // won't work if nums has duplicate vals
+    // use for arrs w/o duplicate vals
     static int findPivot(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
@@ -61,7 +42,7 @@ public class RotatedBS {
         return -1;
     }
 
-    // adjusted for arrs w/ duplicate values
+    // use for arrs w/ duplicate values
     static int findPivotDuplicates(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
@@ -101,25 +82,4 @@ public class RotatedBS {
         }
         return -1;
     }
-
-    static int binarySearch(int[] arr, int target, int start, int end) {
-
-        while (start <= end) {
-            // find middle elem
-            // int mid = (start + end) / 2;
-            // it's possible that start + end exceeds the range for int
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1; // check the left side
-            } else if (target > arr[mid]) {
-                start = mid + 1; // check the right side
-            } else {
-                // ans found
-                return mid;
-            }
-        }
-        return -1;
-    }
 }
-
---
+++ lectures/10-binary search/binary-search-nhi/src/com/nhi/inteviewQs/SplitArray.java
@@ -0,0 +1,53 @@
+package com.nhi.inteviewQs;
+
+/**
+ * Given an _unsorted_ array of non-neg ints and an int m,
+ * split the array into m non-empty continuous subarrays
+ * https://leetcode.com/problems/split-array-largest-sum/
+ */
+public class SplitArray {
+    public static void main(String[] args) {
+
+    }
+
+    public int splitArray(int[] nums, int m) {
+        int start = 0;
+        int end = 0;
+
+        // find start and end
+        for (int i = 0; i < nums.length; i++) {
+            start = Math.max(start, nums[i]); // at the end of the loop, this will contain the max elem
+            end += nums[i];
+        }
+
+        // binary search
+        while (start < end) {
+            // use the middle as a potential ans
+            int mid = start + (end - start) / 2;
+
+            // calculate # pieces we can divide nums into, using this max sum
+            int sum = 0;
+            int pieces = 1;
+
+            for (int num: nums) {
+                if (sum + num > mid) {
+                    // we can't add this to the subarray. make a new one
+                    // new sum = num
+                    sum = num;
+                    pieces++;
+                } else {
+                    // keep appending to subarray
+                    sum += num;
+                }
+            }
+            // condition #1
+            if (pieces > m) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return end; // start = end
+    }
+}
